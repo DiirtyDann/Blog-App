c7a335b4b375b923e19e31405633cda145623059
Trevor M <trevor.munroe@snhu.edu>
2024-12-09 20:31:02 -0500

    small fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login Redirect to home
+LOGIN_REDIRECT_URL = "home"
